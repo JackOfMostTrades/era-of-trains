d08d0db4dcf6f192dada4b7d83d1e69db905fc75
Ian Haken <ihaken@netflix.com>
2024-12-20 18:06:01 -0800

    Update home page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import {Container, Header, List, ListItem} from "semantic-ui-react";
 function Home() {
     return <Container text>
         <Header as='h1'>Era of Trains</Header>
-        <p>Currently only available by invitation.</p>
+        <p>Currently in private beta, so there's not a lot of exposition or tutorial here at present. If you're in the private beta and need help or hit a bug, ping me on Discord.</p>
         <Header as='h2'>Known Issues</Header>
         <List>
             <ListItem>Track replacement of incomplete track is currently not possible</ListItem>
