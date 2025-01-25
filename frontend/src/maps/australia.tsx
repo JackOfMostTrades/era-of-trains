c3f8f77bc83e0218d2f54ae215e572029beed990
Ian Haken <ihaken@netflix.com>
2025-01-25 09:28:17 -0800

    Clarify australia rules.

--- frontend/src/maps/australia.tsx
+++ frontend/src/maps/australia.tsx
@@ -7,7 +7,7 @@ class Australia extends BasicMap {
   public getMapInfo(): ReactNode {
     return <>
       <p>Plays 4-6, best 4-6.</p>
-      <p>Engineering allows you to build one tile for free.</p>
+      <p>Engineering allows you to build one tile for free. (It does not grant you the usual extra build.)</p>
       <p>Urbanization limits you to only 2 builds.</p>
       <p>Delivering to Perth (the Blue white #1 city) earns a bonus 3 income per delivery.</p>
     </>;
