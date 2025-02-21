5ba7cc7606fb55d04feb890a96d814ad5c310073
Ian Haken <ihaken@netflix.com>
2025-02-21 14:16:03 -0800

    Update home and about pages.

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -7,9 +7,9 @@ function About() {
         <Header as='h2'>Privacy Policy</Header>
         <p>Your username and email are collected for purposes of authentication and for notifications. Your actions are logged for the purposes of development and debugging, and are generally available to any users of this site. Any data not otherwise accessible by other site users will never be sold or made available to a third-party unless legally obligated.</p>
         <Header as='h2'>Etiquette</Header>
-        <p>If you are participating in a game, please be kind to everyone else playing and take your turn in a timely manner. If you are playing with others that you do not know outside of this site, we recommend including expectations around play speed in the table name. Stuff happens and sometimes folks cannot play as fast as intended, and this is also fine. However, please refrain from abandoning incomplete games; that makes everyone sad.</p>
+        <p>If you are participating in a game, please be kind to everyone else playing. Keep the discussion friendly; no harassment will be tolerated. Please try and take your turns in a timely manner; if you create a table we recommend including expectations around play speed in the table name. Stuff happens and sometimes folks cannot play as fast as intended. Stuff happens to everyone, just let folks know in the game chat if you'll be delayed. Also, refrain from abandoning incomplete games; that makes everyone sad.</p>
         <Header as='h2'>Contact</Header>
-        <p>Questions, comments, requests? Reach out to eot-at-coderealms-dot-io.</p>
+        <p>Questions, comments, requests? <a target="_blank" href="https://discord.gg/f9rtBrhW2j">Discord</a> is where most chat happens, but you can also reach out with email to eot-at-coderealms-dot-io.</p>
     </Container>
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {Container, Header, List, ListItem} from "semantic-ui-react";
+import {Container, Header} from "semantic-ui-react";
 import {Link} from "react-router";
 
 function Home() {
@@ -7,45 +7,7 @@ function Home() {
         <p>Era of Trains is a site for playing a particular series of train games that are set in the early age of the steam locomotive. This site is a hobby project developed and maintained in free time, and is free for anyone to use. Games on this site are intended for play in an asynchronous, turn-based manner. This site does not have a rules explanation or tutorial mode, but anyone who has played at least once before should find taking their turn easy enough to follow.</p>
         <p>To get started, you can head over to the <Link to='/signin'>sign-in page</Link> and you'll be prompted to create an account. Once you've created your account, head over to the <Link to='/games'>All Games</Link> page. You can also view any finished game or game in progress to get a sense of the interface. From the games page, feel free to join a game that's waiting for players or start a new table.</p>
         <p>The volume of users on this site is low, so organizing players for a game on other social channels is recommended. We've started a dedicated Discord server, which you can join with <a target="_blank" href="https://discord.gg/f9rtBrhW2j">this link</a>! This is a good place to find other players, get notifications if it's your turn, or report issues.</p>
-        <p>The functionality of this site should be considered "beta" as a number of minor issues have been discovered (and fixed) during initial tests. Please proceed with modest expectations.</p>
-        <Header as='h2'>Announcements</Header>
-        <Header as='h3'>Jan 9, 2025</Header>
-        <List bulleted>
-            <ListItem>Add a common Discord turn notifications option so you don't need to run your own server.</ListItem>
-            <ListItem>Add a link to the Discord channel.</ListItem>
-        </List>
-        <Header as='h3'>Jan 7, 2025</Header>
-        <List bulleted>
-            <ListItem>Added support for signing in with Discord.</ListItem>
-            <ListItem>Added support for receiving a Discord webhook when it's your turn.</ListItem>
-            <ListItem>Various minor bugfixes and UI improvements.</ListItem>
-            <ListItem>Text changes to indicate movement to public beta.</ListItem>
-        </List>
-        <Header as='h3'>Dec 30, 2024</Header>
-        <List bulleted>
-            <ListItem>Initial release of the Scotland map is available.</ListItem>
-            <ListItem>Maps have rivers now!</ListItem>
-            <ListItem>Users with pending turns will now receive a daily summary email as a reminder.</ListItem>
-            <ListItem>Cubes glide over the map during move goods action. So fancy!</ListItem>
-        </List>
-        <Header as='h3'>Dec 26, 2024</Header>
-        <List bulleted>
-            <ListItem>Initial release of the Germany map is available.</ListItem>
-            <ListItem>Bankrupt players are now eliminated.</ListItem>
-            <ListItem>Users can now set player color preferences that get applied when starting a new game.</ListItem>
-            <ListItem>Number of steps now displayed during move goods actions.</ListItem>
-            <ListItem>Component limits (track tiles and town markers) are now enforced during builds.</ListItem>
-            <ListItem>Map-specific info is now displayed on game pages.</ListItem>
-        </List>
-        <Header as='h3'>Dec 24, 2024</Header>
-        <List bulleted>
-            <ListItem>Initial release of the Southern U.S. map is available.</ListItem>
-            <ListItem>You can now redirect incomplete tracks during builds.</ListItem>
-            <ListItem>Fixed a bug allowing deliveries to repeat cities.</ListItem>
-            <ListItem>Many other miscellaneous bug fixes and improvements.</ListItem>
-        </List>
-        <Header as='h3'>Dec 20, 2024</Header>
-        <p>Initial private beta release. Let the games begin!</p>
+        <p>Announcements, discussion, and feature requests all happen on Discord, so it's highly recommended that you join us there!</p>
     </Container>
 }
 
