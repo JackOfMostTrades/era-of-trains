231c98ed6145219ead2781634e618714ce2935d5
Ian Haken <ihaken@netflix.com>
2025-02-20 16:21:57 -0800

    Add new cities view.

--- frontend/src/pages/PartsCountComponent.tsx
+++ frontend/src/pages/PartsCountComponent.tsx
@@ -1,9 +1,10 @@
-import {useContext, useState} from "react";
-import {Coordinate, ViewGameResponse} from "../api/api.ts";
+import {ReactNode, useContext, useState} from "react";
+import {Coordinate, GameState, ViewGameResponse} from "../api/api.ts";
 import {
     Accordion,
     AccordionContent,
     AccordionTitle,
+    Container,
     Header,
     Icon,
     Segment,
@@ -14,7 +15,7 @@ import {
 } from "semantic-ui-react";
 import {MapTileState, MapTrackTile, TrackTile, TrackTiles} from "../game/map_state.ts";
 import {GameMap, HexType} from "../maps";
-import {HexRenderer} from "../actions/renderer/HexRenderer.tsx";
+import {HexRenderer, urbCityProperties} from "../actions/renderer/HexRenderer.tsx";
 import UserSessionContext from "../UserSessionContext.tsx";
 
 function TrackTileDisplay({ tile }: {tile: TrackTile}) {
@@ -28,13 +29,7 @@ function TrackTileDisplay({ tile }: {tile: TrackTile}) {
     return renderer.render()
 }
 
-function PartsCountComponent({ map, game }: {map: GameMap, game: ViewGameResponse}) {
-    let [open, setOpen] = useState<boolean>(false);
-
-    if (!game.gameState) {
-        return null;
-    }
-
+function TileCountComponent({ map, gameState }: {map: GameMap, gameState: GameState}) {
     let townMarkerCount = 8
     let tileCount = new Map<TrackTile, number>([
         [TrackTile.STRAIGHT_TRACK_TILE, 48],
@@ -51,7 +46,7 @@ function PartsCountComponent({ map, game }: {map: GameMap, game: ViewGameRespons
         [TrackTile.STRAIGHT_AND_SHARP_TRACK_TILE, 1],
     ]);
 
-    let mapTileState = new MapTileState(map, game.gameState);
+    let mapTileState = new MapTileState(map, gameState);
     for (let y = 0; y < map.getHeight(); y++) {
         for (let x = 0; x < map.getWidth(); x++) {
             let hex: Coordinate = {x: x, y: y};
@@ -70,29 +65,80 @@ function PartsCountComponent({ map, game }: {map: GameMap, game: ViewGameRespons
         }
     }
 
+    return <>
+        <Table compact basic collapsing>
+            <TableBody>
+                {TrackTiles.map((tile,idx) => {
+                    return <TableRow key={idx}>
+                        <TableCell>
+                            <div style={{width: "4em"}}><TrackTileDisplay tile={tile} /></div>
+                        </TableCell>
+                        <TableCell>
+                            {tileCount.get(tile)}
+                        </TableCell>
+                    </TableRow>
+                })}
+            </TableBody>
+        </Table>
+        <p>Town markers: {townMarkerCount}</p>
+    </>
+}
+
+function NewCitiesComponent({gameState}: {gameState: GameState}) {
+    let userSession = useContext(UserSessionContext);
+
+    let columns: ReactNode[] = [];
+    for (let newCityNum = 0; newCityNum < 8; newCityNum++) {
+        let renderer = new HexRenderer(false, false, userSession);
+        renderer.renderCityHex({x: 0, y: 0}, urbCityProperties(newCityNum), undefined);
+
+        let classNames = "track-select";
+        let available = true;
+        if (gameState.urbanizations) {
+            for (let urb of gameState.urbanizations) {
+                if (urb.city === newCityNum) {
+                    available = false;
+                    break;
+                }
+            }
+        }
+
+        if (!available) {
+            classNames += " unavailable";
+        }
+        columns.push(<div className={classNames}>{renderer.render()}</div>)
+    }
+    return <Container>
+        {columns}
+    </Container>
+}
+
+function PartsCountComponent({ map, game }: {map: GameMap, game: ViewGameResponse}) {
+    let [newCitiesOpen, setNewCitiesOpen] = useState<boolean>(false);
+    let [componentCountOpen, setComponentCountOpen] = useState<boolean>(false);
+
+    if (!game.gameState) {
+        return null;
+    }
+
     return <Segment>
         <Header as="h2">Components</Header>
         <Accordion>
-            <AccordionTitle active={open} onClick={() => setOpen(!open)}>
+            <AccordionTitle active={newCitiesOpen} onClick={() => setNewCitiesOpen(!newCitiesOpen)}>
+                <Icon name='dropdown' />
+                New Cities
+            </AccordionTitle>
+            <AccordionContent active={newCitiesOpen} >
+                <NewCitiesComponent gameState={game.gameState} />
+            </AccordionContent>
+        </Accordion>
+        <Accordion>
+            <AccordionTitle active={componentCountOpen} onClick={() => setComponentCountOpen(!componentCountOpen)}>
                 <Icon name='dropdown' />
                 Components Count
             </AccordionTitle>
-            <AccordionContent active={open} >
-                <Table compact basic collapsing>
-                    <TableBody>
-                        {TrackTiles.map((tile,idx) => {
-                            return <TableRow key={idx}>
-                                <TableCell>
-                                    <div style={{width: "4em"}}><TrackTileDisplay tile={tile} /></div>
-                                </TableCell>
-                                <TableCell>
-                                    {tileCount.get(tile)}
-                                </TableCell>
-                            </TableRow>
-                        })}
-                    </TableBody>
-                </Table>
-                <p>Town markers: {townMarkerCount}</p>
+            <AccordionContent active={componentCountOpen} >
+                <TileCountComponent map={map} gameState={game.gameState} />
             </AccordionContent>
         </Accordion>
     </Segment>
